c7c04d9c214fdd427498cf8e0bc85d029ad83c18
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2024-02-13 09:38:43 -0300

    Alterado a url para requisicoes da pagina PME

--- src/config/dbWhatsapp.js
+++ src/config/dbWhatsapp.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host:'localhost',// process.env.DB_HOST_WHATS,
+    host: process.env.DB_HOST_WHATS,
     user: process.env.DB_USER_WHATS,
     password: process.env.DB_PASS_WHATS,
     database: process.env.DB_NAME_WHATS,
