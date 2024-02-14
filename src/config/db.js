bc253ab946ae44e1eac4c84ebd0fd8901a9a3c94
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2024-02-14 18:29:36 -0300

    Alterado mysql2 para mysql

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,32 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-})
+    database: process.env.DB_NAME
+  });
 
-connection.connect((error) => {
-    if (error) throw error
-    console.log('Database connected')
-})
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+      setTimeout(handleDisconnect, 2000); // Tente reconectar após 2 segundos
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.error('Erro de banco de dados:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconecte se a conexão for perdida
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
--- src/config/dbWhatsapp.js
+++ src/config/dbWhatsapp.js
@@ -1,16 +1,33 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST_WHATS,
     user: process.env.DB_USER_WHATS,
     password: process.env.DB_PASS_WHATS,
     database: process.env.DB_NAME_WHATS,
     port: process.env.DB_PORT_WHATS
-})
+  });
 
-connection.connect((error) => {
-    if (error) throw error
-    console.log('Database connected')
-})
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+      setTimeout(handleDisconnect, 2000); // Tente reconectar após 2 segundos
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.error('Erro de banco de dados:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconecte se a conexão for perdida
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
